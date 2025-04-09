cc93f0b795489165375c2db92fcd0d0b0c8f5ce4
hidayahsidiq <muhamads03@gmail.com>
2025-04-09 22:27:03 +0700

    Update main1.py

--- main1.py
+++ main1.py
@@ -9,12 +9,30 @@ import re
 from nltk.corpus import stopwords
 import string
 import openpyxl
+import matplotlib.pyplot as plt
+%matplotlib inline
 
-df = pd.read_excel("D:\Code\Python\Datasheet CERDAS\data siap pakai.xlsx")
-df = df.info()
+df = pd.read_excel("C:/Users/sidiq/Belajar Python/CERDAS AI/data siap pakai.xlsx")
+#print(df.shape)
+#print(df.head())
+
+complaints_df = df.filter(["KETERANGAN_BERSIH","HASIL PREDIKSI"],axis = 1)
+print(complaints_df.head())
+
+print(complaints_df.isnull().sum())
+
+complaints_df.dropna(inplace=True)
+print(complaints_df.shape)
+print(complaints_df.isnull().sum())
+
+print(complaints_df['HASIL PREDIKSI'].value_counts()).plot(kind='pie',autopct='%1.1f%%', startangle=90, colors=['#ff9999','#66b3ff'])
+plt.title('Distribution of Complaint Types')
+"""
+nltk.download('stopwords')
 df['TANGGAL'] = pd.to_datetime(df['TANGGAL'], format='%d-%m-%Y',dayfirst=True)
 print(df['TANGGAL'].head())
 df['KETERANGAN_BERSIH'] = df['KETERANGAN_BERSIH'].astype(str)
 df['KETERANGAN_BERSIH'] = df['KETERANGAN_BERSIH'].str.lower()
 df['KETERANGAN_BERSIH'] = df['KETERANGAN_BERSIH'].str.replace(r'[^a-zA-Z0-9\s]', '', regex=True)
 print(df['KETERANGAN_BERSIH'].head())
+"""
